dbca8d3536468fe190ea526e95db42975b63f388
vagrant <vagrant@localhost.localdomain>
2018-01-07 03:32:51 +0000

    end of chap11

---
+++ Part1/Chapter11/Money.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Part1\Chapter11;
+
+class Money
+{
+    /** @var int */
+    protected $amount;
+
+    /** @var string */
+    protected $currency;
+
+    public function __construct(int $amount, string $currency)
+    {
+        $this->amount = $amount;
+        $this->currency = $currency;
+    }
+
+    /**
+     * @param Money $money
+     * @return bool
+     */
+    public function equals(Money $money): bool
+    {
+        return $this->amount === $money->amount
+            && $this->currency === $money->currency;
+    }
+
+    public static function Dollar(int $amount): Money
+    {
+        return new Money($amount, 'USD');
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Money($amount, 'CHF');
+    }
+
+    public function times(int $multiplier): Money
+    {
+        return new Money($this->amount * $multiplier, $this->currency);
+    }
+
+    /**
+     * @return string
+     */
+    public function currency(): string
+    {
+        return $this->currency;
+    }
+
+    public function __toString(): string
+    {
+        return $this->amount . ' ' . $this->currency;
+    }
+}
